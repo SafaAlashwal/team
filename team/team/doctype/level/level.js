d9a32b588d77babecccfea25088713d445472515
SafaAlashwal <s93887882@gmail.com>
2024-09-25 15:42:16 +0530

    Edit Category

--- team/team/doctype/level/level.js
+++ team/team/doctype/level/level.js
@@ -1,8 +1,27 @@
 // Copyright (c) 2024, team and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Level", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Level", {
+    category : function(frm) {
+        if (frm.doc.category) {
+            frappe.call({
+                method: 'get_specializations',
+                doc : frm.doc,
+                args: {
+                    category: frm.doc.category
+                },
+                callback: function(r) {
+                    if (r.message) {
+                        frm.clear_table('sub_category');  // assuming 'specializations' is the table field in Inquiry
+                        r.message.forEach(function(spec) {
+                            let row = frm.add_child('sub_category');
+                            row.name1 = spec.name1;
+                            row.number = spec.number;
+                        });
+                        frm.refresh_field('sub_category');
+                    }
+                }
+            });
+        }
+    }
+});
--- team/team/doctype/level/level.py
+++ team/team/doctype/level/level.py
@@ -1,9 +1,21 @@
 # Copyright (c) 2024, team and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class Level(Document):
-	pass
+	@frappe.whitelist()
+	def get_specializations(self, category):
+		college_doc = frappe.get_doc("Category", category)
+		specializations = []
+		
+		for spec in college_doc.sub_category:  # assuming 'specializations' is the table field
+			specializations.append({
+				"name1": spec.name1,  # assuming these are the fields in the table
+				"number": spec.number
+			})
+
+		return specializations
+
