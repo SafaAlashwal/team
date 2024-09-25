b1de7d5b636d65a5fb265a5935a5733e724a6d3a
frappe <SafaAlashwal@gmail.com>
2024-09-25 15:54:47 +0530

    edit doctype level

--- team/team/doctype/level/level.js
+++ team/team/doctype/level/level.js
@@ -6,3 +6,33 @@
 
 // 	},
 // });
+frappe.ui.form.on('Level', {
+    category: function(frm) {
+        if (frm.doc.category) {
+            frappe.call({
+                method: 'frappe.client.get_list',
+                args: {
+                    doctype: 'Sub Category',
+                    filters: {
+                        'category': frm.doc.category
+                    },
+                    fields: ['name', 'category']
+                },
+                callback: function(response) {
+                    var sub_categories = response.message;
+                    if (sub_categories) {
+                        frm.clear_table('sup_category_table');
+
+                        sub_categories.forEach(function(sub_category) {
+                            var row = frm.add_child('sup_category_table');
+                            row.sup_category = sub_category.name;
+                        });
+
+                        frm.refresh_field('sup_category_table');
+                    }
+                }
+            });
+        }
+    }
+});
+
