5afb3a673e41abbbe41e8ce7b392c32ee128d901
frappe <SafaAlashwal@gmail.com>
2024-10-04 22:27:09 +0530

    Edit doctype Safa

--- team/team/doctype/safa/safa.js
+++ team/team/doctype/safa/safa.js
@@ -1,8 +1,8 @@
 // Copyright (c) 2024, team and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Safa", {
-// 	refresh(frm) {
+frappe.ui.form.on("Safa", {
+	refresh(frm) {
 
-// 	},
-// });
+	},
+});
